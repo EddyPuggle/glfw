1ab0a42ebb89f5c2ad84c88d575434ba2a02bcc0
EddyPuggle <werkmeister.elton2603@gmx.de>
2023-08-22 23:51:14 +0200

    fixes typo in multiplatform files config

--- premake5.lua
+++ premake5.lua
@@ -1,4 +1,4 @@
-project "GLFW"
+project "glfw"
     kind "staticLib"
     language "C"
 
@@ -10,7 +10,7 @@ project "GLFW"
         "include/GLFW/glfw3.h",
         "include/GLFW/glfw3native.h",
         "src/glfw_config.h",
-        "src/contect.c",
+        "src/context.c",
         "src/init.c",
         "src/input.c",
         "src/monitor.c",
