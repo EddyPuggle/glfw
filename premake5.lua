a1ffd412a449b92c1222a02f03450087ab21e842
EddyPuggle <werkmeister.elton2603@gmx.de>
2023-08-23 02:00:02 +0200

    updates glfw´s premake5 build file

--- premake5.lua
+++ premake5.lua
@@ -1,4 +1,4 @@
-project "glfw"
+project "GLFW"
     kind "staticLib"
     language "C"
 
@@ -9,12 +9,25 @@ project "glfw"
     files {
         "include/GLFW/glfw3.h",
         "include/GLFW/glfw3native.h",
-        "src/glfw_config.h",
+        "src/internal.h",
+        "src/platform.h",
+        "src/mappings.h",
+        "src/null_platform.h",
+        "src/null_joystick.h",
         "src/context.c",
-        "src/init.c",
-        "src/input.c",
+        "src/vulkan.c",
+        "src/platform.c",
         "src/monitor.c",
-        "src/window.c"
+        "src/window.c",
+        "src/egl_context.c",
+        "src/osmesa_context.c",
+        "src/init.c",
+        "src/null_init.c",
+        "src/input.c",
+    
+        "src/null_monitor.c",
+        "src/null_window.c",
+        "src/null_joystick.c",
     }
 
     filter "system:windows"
@@ -22,11 +35,16 @@ project "glfw"
         staticruntime "On"
     -- Windows OS specific files
         files {
+            "src/win32_joystick.h",
+            "src/win32_platform.h",
+            "src/win32_thread.h",
+            "src/win32_time.h",
             "src/win32_init.c",
             "src/win32_joystick.c",
+            "src/win32_module.c",
             "src/win32_monitor.c",
-            "src/win32_time.c",
             "src/win32_thread.c",
+            "src/win32_time.c",
             "src/win32_window.c",
             "src/wgl_context.c",
             "src/egl_context.c",
@@ -51,7 +69,6 @@ project "glfw"
             "src/posix_module.c",
             "src/posix_time.c",
             "src/posix_thread.c",
-            "src/posix_module.c",
             "src/glx_context.c",
             "src/egl_context.c",
             "src/osmesa_context.c",
